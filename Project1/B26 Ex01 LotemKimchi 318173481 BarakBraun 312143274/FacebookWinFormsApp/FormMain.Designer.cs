afdc4cd57b9be4930f45fe0b568a4cbfc24b8841
LotemKimchi <lotem1237@gmail.com>
2026-03-14 17:43:32 +0200

    project 1

---
+++ Project1/B26 Ex01 LotemKimchi 318173481 BarakBraun 312143274/FacebookWinFormsApp/FormMain.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace BasicFacebookFeatures
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonLogout = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.listBoxFriend = new System.Windows.Forms.ListBox();
+            this.labelBirthday = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.buttonConnectAsDesig = new System.Windows.Forms.Button();
+            this.pictureBoxProfile = new System.Windows.Forms.PictureBox();
+            this.textBoxAppID = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.listBoxAlbums = new System.Windows.Forms.ListBox();
+            this.labelFriendsList = new System.Windows.Forms.Label();
+            this.labelAlbumList = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfile)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(18, 17);
+            this.buttonLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(268, 44);
+            this.buttonLogin.TabIndex = 36;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // buttonLogout
+            // 
+            this.buttonLogout.Enabled = false;
+            this.buttonLogout.Location = new System.Drawing.Point(18, 121);
+            this.buttonLogout.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonLogout.Name = "buttonLogout";
+            this.buttonLogout.Size = new System.Drawing.Size(268, 43);
+            this.buttonLogout.TabIndex = 52;
+            this.buttonLogout.Text = "Logout";
+            this.buttonLogout.UseVisualStyleBackColor = true;
+            this.buttonLogout.Click += new System.EventHandler(this.buttonLogout_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(314, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(397, 72);
+            this.label1.TabIndex = 53;
+            this.label1.Text = "This is the AppID of \"Design Patterns App 2.4\".\r\nThe grader will use it to test y" +
+    "our app.\r\nType here your own AppID to test it:\r\n";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(862, 370);
+            this.tabControl1.TabIndex = 54;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.labelAlbumList);
+            this.tabPage1.Controls.Add(this.labelFriendsList);
+            this.tabPage1.Controls.Add(this.listBoxAlbums);
+            this.tabPage1.Controls.Add(this.listBoxFriend);
+            this.tabPage1.Controls.Add(this.labelBirthday);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.labelName);
+            this.tabPage1.Controls.Add(this.buttonConnectAsDesig);
+            this.tabPage1.Controls.Add(this.pictureBoxProfile);
+            this.tabPage1.Controls.Add(this.textBoxAppID);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.buttonLogout);
+            this.tabPage1.Controls.Add(this.buttonLogin);
+            this.tabPage1.Location = new System.Drawing.Point(4, 31);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(854, 335);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // listBoxFriend
+            // 
+            this.listBoxFriend.FormattingEnabled = true;
+            this.listBoxFriend.ItemHeight = 22;
+            this.listBoxFriend.Location = new System.Drawing.Point(319, 193);
+            this.listBoxFriend.Name = "listBoxFriend";
+            this.listBoxFriend.Size = new System.Drawing.Size(173, 136);
+            this.listBoxFriend.TabIndex = 61;
+            // 
+            // labelBirthday
+            // 
+            this.labelBirthday.AutoSize = true;
+            this.labelBirthday.Location = new System.Drawing.Point(163, 171);
+            this.labelBirthday.Name = "labelBirthday";
+            this.labelBirthday.Size = new System.Drawing.Size(117, 24);
+            this.labelBirthday.TabIndex = 59;
+            this.labelBirthday.Text = "labelBirthday";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(167, 171);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(123, 78);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 58;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(14, 171);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(101, 24);
+            this.labelName.TabIndex = 57;
+            this.labelName.Text = "labelName";
+            this.labelName.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // buttonConnectAsDesig
+            // 
+            this.buttonConnectAsDesig.Location = new System.Drawing.Point(18, 69);
+            this.buttonConnectAsDesig.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonConnectAsDesig.Name = "buttonConnectAsDesig";
+            this.buttonConnectAsDesig.Size = new System.Drawing.Size(268, 44);
+            this.buttonConnectAsDesig.TabIndex = 56;
+            this.buttonConnectAsDesig.Text = "Connect As Desig";
+            this.buttonConnectAsDesig.UseVisualStyleBackColor = true;
+            this.buttonConnectAsDesig.Click += new System.EventHandler(this.buttonConnectAsDesig_Click);
+            // 
+            // pictureBoxProfile
+            // 
+            this.pictureBoxProfile.Location = new System.Drawing.Point(18, 171);
+            this.pictureBoxProfile.Name = "pictureBoxProfile";
+            this.pictureBoxProfile.Size = new System.Drawing.Size(119, 78);
+            this.pictureBoxProfile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxProfile.TabIndex = 55;
+            this.pictureBoxProfile.TabStop = false;
+            // 
+            // textBoxAppID
+            // 
+            this.textBoxAppID.Location = new System.Drawing.Point(319, 126);
+            this.textBoxAppID.Name = "textBoxAppID";
+            this.textBoxAppID.Size = new System.Drawing.Size(446, 28);
+            this.textBoxAppID.TabIndex = 54;
+            this.textBoxAppID.Text = "1450160541956417";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 31);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(854, 335);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // listBoxAlbums
+            // 
+            this.listBoxAlbums.FormattingEnabled = true;
+            this.listBoxAlbums.ItemHeight = 22;
+            this.listBoxAlbums.Location = new System.Drawing.Point(524, 193);
+            this.listBoxAlbums.Name = "listBoxAlbums";
+            this.listBoxAlbums.Size = new System.Drawing.Size(309, 136);
+            this.listBoxAlbums.TabIndex = 62;
+            // 
+            // labelFriendsList
+            // 
+            this.labelFriendsList.AutoEllipsis = true;
+            this.labelFriendsList.AutoSize = true;
+            this.labelFriendsList.Location = new System.Drawing.Point(315, 171);
+            this.labelFriendsList.Name = "labelFriendsList";
+            this.labelFriendsList.Size = new System.Drawing.Size(106, 24);
+            this.labelFriendsList.TabIndex = 63;
+            this.labelFriendsList.Text = "Friends List";
+            this.labelFriendsList.Click += new System.EventHandler(this.labelBoxFriends_Click);
+            // 
+            // labelAlbumList
+            // 
+            this.labelAlbumList.AutoEllipsis = true;
+            this.labelAlbumList.AutoSize = true;
+            this.labelAlbumList.Location = new System.Drawing.Point(520, 171);
+            this.labelAlbumList.Name = "labelAlbumList";
+            this.labelAlbumList.Size = new System.Drawing.Size(106, 24);
+            this.labelAlbumList.TabIndex = 64;
+            this.labelAlbumList.Text = "Albums List";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 370);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfile)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonLogin;
+		private System.Windows.Forms.Button buttonLogout;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBoxAppID;
+        private System.Windows.Forms.PictureBox pictureBoxProfile;
+        private System.Windows.Forms.Button buttonConnectAsDesig;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelBirthday;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBoxFriend;
+        private System.Windows.Forms.ListBox listBoxAlbums;
+        private System.Windows.Forms.Label labelAlbumList;
+        private System.Windows.Forms.Label labelFriendsList;
+    }
+}
+
---
+++ Project1/B26 Ex01 LotemKimchi 318173481 BarakBraun 312143274/FacebookWinFormsApp/FormMain.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using FacebookWrapper.ObjectModel;
+using FacebookWrapper;
+
+namespace BasicFacebookFeatures
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            FacebookWrapper.FacebookService.s_CollectionLimit = 25;
+        }
+
+        FacebookWrapper.LoginResult m_LoginResult;
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            m_LoginResult = FacebookWrapper.FacebookService.Login(
+                "1783124789311728",
+                "email",
+                "public_profile",
+                "user_friends");
+
+            Clipboard.SetText("design.patterns");
+
+            if (!string.IsNullOrEmpty(m_LoginResult.AccessToken))
+            {
+                MessageBox.Show($"Logged in as {m_LoginResult.LoggedInUser.Name}");
+                displayUserInfo();
+            }
+            else
+            {
+                MessageBox.Show(m_LoginResult.ErrorMessage);
+            }
+            //if (m_LoginResult == null)
+            //{
+            //    login();
+            //}
+        }
+
+        //אולי צריך להוסיף  לסעיף G
+        private void displayUserInfo()
+        {
+            User user = m_LoginResult.LoggedInUser;
+
+            labelName.Text = user.Name;
+            labelBirthday.Text = user.Birthday;
+
+            labelFriendsList.Text = "Friends List";
+            listBoxFriend.Items.Clear();
+
+            foreach (User friend in user.Friends)
+            {
+                listBoxFriend.Items.Add(friend.Name);
+            }
+
+            labelAlbumList.Text = "Album List";
+            listBoxAlbums.Items.Clear();
+
+            foreach (Album album in user.Albums)
+            {
+                listBoxAlbums.Items.Add(album.Name);
+            }
+        }
+        private void login()
+        {
+            m_LoginResult = FacebookService.Login(
+                /// (This is Desig Patter's App ID. replace it with your own)
+                textBoxAppID.Text,
+                /// requested permissions:
+                "email",
+                "public_profile"
+                /// add any relevant permissions
+                );
+
+            if (string.IsNullOrEmpty(m_LoginResult.ErrorMessage))
+            {
+                afterLogin();
+            }
+
+            User user = m_LoginResult.LoggedInUser;
+
+        }
+
+
+        private void buttonConnectAsDesig_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                m_LoginResult = FacebookService.Connect("EAAUm6cZC4eUEBQTAa3rRgO39UZCIJLeD9OpF5SYAevqSaFI16sfjT6JznpAUbyX5Soyj4Uv2ZBRkesoHO9omNcJ3KSYPZCExgaKrIprACUMIVnhiHzT5a46zbdC2VkvZC04n1ZARj8WmvOCYyuIdmRZBNjtWZCFJrbjFoms5t3sU8G9dO1xDCYH7kkfU67heIUZCFDIuTtL0CzF2JUHBpRpwPdXYilOJW811z3C5fY9TOyBiUwZAqx4ZAV6YS5ZBBtYKdsb7");
+                displayUserInfo();
+                afterLogin();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(m_LoginResult.ErrorMessage, "Login Failed");
+            }
+        }
+
+        private void afterLogin()
+        {
+
+            buttonLogin.Text = $"Logged in as {m_LoginResult.LoggedInUser.Name}";
+            buttonLogin.BackColor = Color.LightGreen;
+            pictureBoxProfile.ImageLocation = m_LoginResult.LoggedInUser.PictureNormalURL;
+            buttonLogin.Enabled = false;
+            buttonLogout.Enabled = true;
+        }
+
+        private void buttonLogout_Click(object sender, EventArgs e)
+        {
+            FacebookService.LogoutWithUI();
+            buttonLogin.Text = "Login";
+            buttonLogin.BackColor = buttonLogout.BackColor;
+            m_LoginResult = null;
+            buttonLogin.Enabled = true;
+            buttonLogout.Enabled = false;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelBoxFriends_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
